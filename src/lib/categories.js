5ea64f1840df86058780c097f4adc9f94d0a5474
Executive_Eagle <christophershelley257@gmail.com>
2026-06-18 15:45:49 -0500

    Add finance categories

---
+++ src/lib/categories.js
@@ -0,0 +1,35 @@
+export const transactionCategories = [
+  'Gas',
+  'Food',
+  'Car Payment',
+  'Insurance',
+  'Phone',
+  'Giving',
+  'Savings',
+  'Personal',
+  'School',
+  'Emergency',
+  'Income',
+  'Other',
+];
+
+export const billCategories = [
+  'Car',
+  'Insurance',
+  'Phone',
+  'Food',
+  'Gas',
+  'Giving',
+  'Subscription',
+  'School',
+  'Other',
+];
+
+export const transactionTypes = [
+  { value: 'income', label: 'Income' },
+  { value: 'expense', label: 'Expense' },
+  { value: 'transfer', label: 'Transfer' },
+  { value: 'car-payment', label: 'Car Payment' },
+  { value: 'goal-contribution', label: 'Goal Contribution' },
+  { value: 'adjustment', label: 'Balance Adjustment' },
+];
